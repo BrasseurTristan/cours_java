933cce4858c18cc9fc0f5fe662c3fe1283234346
BrasseurTristan <brasseur.tristan@gmail.com>
2025-02-17 13:52:31 +0100

    Les types Booléens

---
+++ src/TypesBoolean.java
@@ -0,0 +1,27 @@
+public class TypesBoolean {
+    public static void main(String[] args) {
+
+        // Un booléen répond soit vrai(true) soit faux(false)
+        boolean state1 = true;
+        System.out.println(state1);
+        boolean state2 = false;
+        System.out.println(state2);
+
+        // Opérateurs booléens : &&(and) ||(or) !(not)
+        // L'opérateur &&
+        if (state1 && state2) {
+            System.out.println("Les deux variables sont initialisées à true ");
+        }
+        ;
+        // L'opérateur ||
+        if (state1 || state2) {
+            System.out.println("L'une des deux variables est initialisée à true ");
+        }
+        ;
+        // L'opérateur !
+        if (!state1) {
+            System.out.println("state1 est initialisée à false ");
+        }
+        ;
+    }
+}
